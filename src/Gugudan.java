7214ac7cd1935929d4f97b77f20d64b830d5ecb9
heeju <myhopeagape@naver.com>
2023-01-26 14:41:58 +0900

    "test" add

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -3,6 +3,6 @@ public class Gugudan {
 	public static void main(String[] args) {
 		System.out.println("4단");
 		int result = 4 * 1;
-		System.out.println(result);
+		System.out.println("test");
 	}	
 }
