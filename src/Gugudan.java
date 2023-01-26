c9e3b8d847d89d3f92e86f6614aa8ed53b02cea0
heeju <myhopeagape@naver.com>
2023-01-26 14:48:55 +0900

    Scanner

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
-		System.out.println("4단");
-		int result = 4 * 1;
-		System.out.println("test");
+		System.out.println("구구단 중 출력할 단은?");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("number : " + number);
+
+        int result = number * 1; 
 	}	
 }
