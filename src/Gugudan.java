05f67e1ab66e684cb686896347388e7bad09a363
heeju <myhopeagape@naver.com>
2023-01-26 14:58:06 +0900

    Scanner number 변수

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -5,8 +5,13 @@ public class Gugudan {
 		System.out.println("구구단 중 출력할 단은?");
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        System.out.println("number : " + number);
-
-        int result = number * 1; 
-	}	
-}
+        System.out.println(number * 1);
+        System.out.println(number * 2);
+        System.out.println(number * 3);
+        System.out.println(number * 4);
+        System.out.println(number * 5);
+        System.out.println(number * 6);
+        System.out.println(number * 7);
+        System.out.println(number * 8);
+        System.out.println(number * 9);
+	}
